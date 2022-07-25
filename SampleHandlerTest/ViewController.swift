b0fd479c83f193dd7147e79854a51bd080ba70dc
jass <jaswant0605@gmail.com>
2022-07-25 16:50:42 +0530

    Ability to share frames from sample handler to UI

--- SampleHandlerTest/ViewController.swift
+++ SampleHandlerTest/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import ReplayKit
 
 class ViewController: UIViewController {
 
+    private var broadcastPickerView: RPSystemBroadcastPickerView? = nil
+    private var serverSocket: RongRTCServerSocket?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setupServerSocket()
+        self.setupReplayKitPicker()
     }
+    
+    private func setupReplayKitPicker() {
+        serverSocket?.createServerSocket() 
+        broadcastPickerView = RPSystemBroadcastPickerView.init(frame: CGRect(x: 0, y: 64, width: UIScreen.main.bounds.size.width, height: 80))
+        broadcastPickerView?.preferredExtension = "com.thirdeyegen.mrworkspace.SampleHandlerTest.SampleHandlerTestExtension"
+        broadcastPickerView?.backgroundColor = UIColor(red: 53.0/255, green: 129.0/255, blue: 242.0/255, alpha: 1.0 )
+        broadcastPickerView?.showsMicrophoneButton = false
+        self.view.addSubview(broadcastPickerView!)
+    }
+    
+    private func setupServerSocket() {
+        if(serverSocket == nil) {
+            let socket = RongRTCServerSocket()
+            socket.delegate = self
+            serverSocket = socket
+        }
+    }
+}
 
-
+extension ViewController: RongRTCServerSocketProtocol {
+    func didProcessSampleBuffer(_ sampleBuffer: CMSampleBuffer) {
+        print("Frame Captured")
+    }
 }
 
---
+++ SampleHandlerTestExtension/SampleHandler.swift
@@ -0,0 +1,54 @@
+//
+//  SampleHandler.swift
+//  SampleHandlerTestExtension
+//
+//  Created by Jaswant Singh on 25/07/22.
+//
+
+import ReplayKit
+
+class SampleHandler: RPBroadcastSampleHandler {
+    
+    private var clientSocket: RongRTCClientSocket?
+    
+    
+    override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
+        // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional.
+        clientSocket = RongRTCClientSocket()
+        clientSocket?.createCliectSocket()
+    }
+    
+    override func broadcastPaused() {
+        // User has requested to pause the broadcast. Samples will stop being delivered.
+    }
+    
+    override func broadcastResumed() {
+        // User has requested to resume the broadcast. Samples delivery will resume.
+    }
+    
+    override func broadcastFinished() {
+        // User has requested to finish the broadcast.
+    }
+    
+    override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
+        switch sampleBufferType {
+        case RPSampleBufferType.video:
+            // Handle video sample buffer
+            sendData(sampleBuffer)
+            break
+        case RPSampleBufferType.audioApp:
+            // Handle audio sample buffer for app audio
+            break
+        case RPSampleBufferType.audioMic:
+            // Handle audio sample buffer for mic audio
+            break
+        @unknown default:
+            // Handle other sample buffer types
+            fatalError("Unknown type of sample buffer")
+        }
+    }
+    
+    func sendData(_ sampleBuffer: CMSampleBuffer) {
+        self.clientSocket?.encode(sampleBuffer)
+    }
+}
